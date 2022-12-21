1a825c08786d54e16497e3aef808219e1f8bb508
Sad Pencil <sadpencil@outlook.com>
2022-12-22 00:39:00 +0800

    Update checkout-all.sh

--- checkout-all.sh
+++ checkout-all.sh
@@ -3,7 +3,7 @@ set -e
 
 output_dir="/tmp/my_output"
 for commit in `git log --format="format:%H"`; do
-	dir="$output_dir/$commit/"
+	dir="$output_dir/$commit/";
 	mkdir -p "$dir";
 	git archive --format=tar $commit | tar -xf - -C "$dir";
 done
